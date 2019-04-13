539cb8d22c17aeea4dc4a3060f33f683c2c3670a
Orestes Polyzos <orestes.polyzos@gmail.com>
2019-04-13 23:21:55 +0300

    Adds demonstration gif

--- src/main/java/ore/plugins/idea/lib/exception/CancelException.java
+++
@@ -1,6 +0,0 @@
-package ore.plugins.idea.lib.exception;
-
-import ore.plugins.idea.lib.exception.base.OrePluginRuntimeException;
-
-public class CancelException extends OrePluginRuntimeException {
-}
